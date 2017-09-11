476b92ecde8e05f562df1fb0b5f8fe3c9f3ce597
matiduarte <duarte.mati@gmail.com>
2017-09-11 15:41:46 -0300

    First assingmnet complete, two news componets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
