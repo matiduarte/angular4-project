96b9e32179427a80bd99839e87d0748205e3464d
matiduarte <duarte.mati@gmail.com>
2017-09-12 16:07:09 -0300

    Assignment 3 Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles:[`
+		.click {
+			color: white;
+		}`]
 })
 export class AppComponent {
+
+	btnClick = false;
+	clicks = [];
+	count_click = 1;
+
+	onClickButton(){
+		this.btnClick = !this.btnClick;
+		this.clicks.push(this.count_click);
+		this.count_click = this.count_click + 1;
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningalertComponent } from './warningalert/warningalert.component';
-import { SuccessalertComponent } from './successalert/successalert.component';
-
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningalertComponent,
-    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
